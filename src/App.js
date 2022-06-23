04c1db3548c5cfc188056dee05301b05b5957024
shubham V <Shubham.V@cerner.com>
2022-06-23 15:19:17 +0530

    input fiedl by use state and ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Todos from "./components/Todos";
+import FormInput from "./components/FormInput";
+
+import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
+  //const [username, setUsername] = useState("")
+  const usernameRef = useRef();
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(usernameRef)
+  }
+
   return (
-    <div className="App">
-      <Todos/>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <FormInput placeholder="Username" refer={usernameRef}/>
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="password"/>
+        <FormInput placeholder="something else"/>
+        <button>submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.js
@@ -0,0 +1,16 @@
+import React  from "react";
+
+import './formInput.css';
+
+const FormInput = (props) => {
+    return (
+        <div className="formInput">
+            {/* <label>Username</label> */}
+            <input 
+            ref={props.refer}
+            placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default FormInput;
