563dbbde09a9e80716cbee0c1e748a81e3b14cd7
shubham V <Shubham.V@cerner.com>
2022-06-24 23:19:05 +0530

    search by filter multiple

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import './App.css'
-import {Users} from './user'
+import Table from './Table';
+import { Users } from './user'
 
 function App() {
   const [query, setQuery] = useState('');
+ const keys = ["first_name", "last_name", "email"];
+
+
+  const search = (data) => {
+    return data.filter((item) =>
+      // item.first_name.toLowerCase().includes(query) ||
+      // item.last_name.toLowerCase().includes(query) ||
+      // item.email.toLowerCase().includes(query))
+      keys.some((key)=> item[key].toLowerCase().includes(query)))
+  }
+
   return (
     <div className="app">
-      <input type="text" placeholder='search...' className='search' onChange={e => setQuery(e.target.value)}/>
-      <ul className='list'>
-        {Users.filter((value)=> value.first_name.toLowerCase().includes(query)).map((user)=>(
-          <li key={user.id} className='listItem'>{user.first_name}</li>
-        ))}
-      </ul>
+      <input type="text" placeholder='search...' className='search' onChange={e => setQuery(e.target.value)} />
+      <Table data={search(Users)} />
     </div>
   );
 }
