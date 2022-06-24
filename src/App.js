f443d6c84b227cb92ff11bf7ffeff8a3894780dc
shubham V <Shubham.V@cerner.com>
2022-06-24 19:57:38 +0530

    basic filter search function

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
-import FormInput from "./components/FormInput";
-
+import { useState } from 'react';
 import './App.css'
-import { useState } from "react";
+import {Users} from './user'
 
 function App() {
-  const [values, setValues] = useState({
-    username:"",
-    email:"",
-    birthday:"",
-    password:"",
-    confirmpassword:""
-  });
-
-  const inputs = [
-    {
-      id:1,
-      name:"username",
-      type:"text",
-      placeholder:"Username",
-      label:"Username",
-      errorMessage:  "Username should be 3-16 characters and shouldn't include any special character!",
-      required: true,
-      pattern: "^[A-Za-z0-9]{3,16}$",
-    },
-    {
-      id:2,
-      name:"email",
-      type:"email",
-      placeholder:"Email",
-      label:"Email",
-      errorMessage: "It should be a valid email address!",
-      required: true
-    },
-    {
-      id:3,
-      name:"birthday",
-      type:"date",
-      placeholder:"Birthday",
-      label:"Birthday"
-    },
-    {
-      id:4,
-      name:"password",
-      type:"password",
-      placeholder:"Password",
-      label:"Password",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-        required: true,
-        pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-    },
-    {
-      id:5,
-      name:"confirmpassword",
-      type:"text",
-      placeholder:"Confirmpassword",
-      label:"Confirmpassword",
-      errorMessage: "Passwords don't match!",
-      required: true,
-      pattern: values.password,
-    }
-  ]
-
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-  }
-
-  const onChange = (e) =>{
-    setValues({...values, [e.target.name]: e.target.value})
-  }
-
-  console.log(values)
+  const [query, setQuery] = useState('');
   return (
     <div className="app">
-      <form onSubmit={handleSubmit}>
-      <h1>Register</h1>
-        {inputs.map((input)=>{
-          return <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
-        })}
-        
-        <button>submit</button>
-      </form>
+      <input type="text" placeholder='search...' className='search' onChange={e => setQuery(e.target.value)}/>
+      <ul className='list'>
+        {Users.filter((value)=> value.first_name.toLowerCase().includes(query)).map((user)=>(
+          <li key={user.id} className='listItem'>{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
