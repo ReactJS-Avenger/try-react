304f8d6adf032a94c91d02936b194ca42d42200d
shubham V <Shubham.V@cerner.com>
2022-07-15 11:42:16 +0530

    react auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Todos from "./components/Todos";
+
 
 function App() {
   return (
-    <div className="App">
-      <Todos/>
-    </div>
+    <main className="App">
+     
+    </main>
   );
 }
 
