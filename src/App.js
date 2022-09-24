dc7d1102c3c810cd4b95d9c9b621b9f43af41e8d
shubham V <Shubham.V@cerner.com>
2022-09-24 20:33:38 +0530

    Intro to hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      <Todos/>
+      shubham
     </div>
   );
 }
