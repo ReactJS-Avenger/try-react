36be598cd4d2b340bf87910ca4757dd965f0676a
shubham V <Shubham.V@cerner.com>
2022-09-29 15:22:06 +0530

    useEffect: not optical since it runs on count and name change. we need to add condtion

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -3,17 +3,21 @@ import { useEffect, useState } from "react";
 const HookCounterOne = () => {
 
     const [count, setCounter] = useState(0);
+    const [name, setName] = useState('')
 
     // to useEffect we pass the function which get executed after every render of the component.i.e useEffect runs after every render.
     // it runs on first render and after every update.
     // useEffect is used inside the component. So that we can have access both state and props.
     // this is how we can use useEffect to cause side effect in component.
+    // useEffect run every render might cause problem. So we need to conditionally run effect
     useEffect(() => {
+        console.log('useEffect updating document title');
         document.title = `You clicked ${count} times`
     })
 
     return (
         <div>
+            <input type='text' value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCounter(count + 1)}>Count {count}</button>
         </div>
     )
