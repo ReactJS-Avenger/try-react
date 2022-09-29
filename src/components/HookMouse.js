5cd643d971a4974879da6fe06161d2e15083f21e
shubham V <Shubham.V@cerner.com>
2022-09-29 16:09:08 +0530

    useEffect: cleanup code or unmountcode

--- src/components/HookMouse.js
+++ src/components/HookMouse.js
@@ -11,10 +11,23 @@ const HookMouse = () => {
     }
 
     //effect is called after everny render.unless we specify the dependecy
+    // we can pass cleanup function to the fuction passed to useeffect, which retun fucntion is cleanup fucntion, runs after component unmount
+    
     useEffect(() => {
         console.log('useEffect called');
         window.addEventListener('mousemove', logMousePoistion)
+
+        //whatevern we return from here will be executed once the compoent unmounts
+        // cleanup fucntions
+        return () => {
+            console.log('component unmount cleanup code');
+            window.removeEventListener('mousemove', logMousePoistion)
+        }
+
     }, [])
+
+    // we can not removed the event attched this will result in memory leak and warnign in the console
+    // make sure once you unmount the component, you remove all subscritpion/ events attached
 
     return (
         <div>
