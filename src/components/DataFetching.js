275a923dcdbbbd4f0f0254ea8d60be67bbc62b98
shubham V <Shubham.V@cerner.com>
2022-09-29 19:15:09 +0530

    useEffect: featch api with axios1 onChange

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const DataFetching = () => {
-    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState({});
+    const [id, setId] = useState(1);
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(({data}) => {
-                setPosts(data)
+                setPost(data)
             }).catch(err => {
                 console.log(err);
             })
-    },[])
+    },[id])
     //without [] it will make inifinite api call on each render
 
     return (
         <div>
+            <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+            <div>{post.title}</div>
             <ul>
-                {
+                {/* {
                     posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
+                } */}
             </ul>
         </div>
     )
