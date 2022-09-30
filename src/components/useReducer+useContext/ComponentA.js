7e397d514c7c27a4d3bf3509b9ff6d6cdb4a44f1
shubham V <Shubham.V@cerner.com>
2022-09-30 19:55:28 +0530

    useReducer- dispaly state in each component

--- src/components/useReducer+useContext/ComponentA.js
+++ src/components/useReducer+useContext/ComponentA.js
@@ -6,7 +6,7 @@ const ComponentA = () => {
     const countContext = useContext(CountContext);
     return (
         <div>
-            Component A
+            Component A - {countContext.countState}
             <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
             <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
             <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
